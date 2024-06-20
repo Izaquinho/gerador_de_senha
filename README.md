e2cc6c7b87f2bfc5d4a28211931ad7fcea535669
Isaak dos Reis <102633982+Izaquinho@users.noreply.github.com>
2024-06-20 11:44:09 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center"> Gerador de Senha</h1>
+
+Este script em Python gera senhas seguindo o formato especificado: "3 letras (abc) + 2 caracteres + 4 dígitos de datas (dia e mês)".
+
+## 🗒 Descrição do Formato
+
+A senha gerada segue o formato:
+
+- `3 letras`: Letras selecionadas aleatoriamente entre letras definidas pelo usuário, exemplo ( a , b , c).
+- `2 caracteres`: Caracteres especiais selecionados aleatoriamente.
+- `4 dígitos de datas (dia e mês)`: Combinações de dia e mês geradas aleatoriamente.
+  
+<details>
+    
+<summary>Exemplo</summary>
+
+- Um exemplo de senha gerada pelo script pode ser: abc$^1106.
+
+</details>
+
+## ⚙️ Requisitos
+
+- Python 3.x
+
+## ✏️ Personalização
+
+- Você pode personalizar o script para alterar o conjunto de caracteres para letras e caracteres especiais conforme suas necessidades. O script utiliza conjuntos pré-definidos para simplicidade.
---
+++ gerador_de_senha.py
@@ -0,0 +1,37 @@
+﻿import random
+
+def gerar_senha():
+    # Gera uma sequência específica para A, B, C (em maiúsculas ou minúsculas)
+    letras = ''.join([
+        random.choice(['a', 'A']),
+        random.choice(['b', 'B']),
+        random.choice(['c', 'C'])
+    ])
+    
+    # Conjunto de caracteres especiais permitidos
+    caracteres_especiais = "!#*.?@_"
+    
+    # Escolhe dois caracteres especiais aleatórios
+    caracteres = ''.join(random.choice(caracteres_especiais) for _ in range(2))
+    
+    # Gera dias e meses no formato DDMM
+    dia = f"{random.randint(1, 31):02d}"
+    mes = f"{random.randint(1, 12):02d}"
+    data = dia + mes
+
+    # Combina todas as partes para formar a senha
+    senha = letras + caracteres + data
+
+    return senha
+
+def main():
+    while True:
+        senha_gerada = gerar_senha()
+        print("Senha gerada:", senha_gerada)
+        
+        resposta = input("Deseja gerar outra senha? (s/n): ").strip().lower()
+        if resposta != 's':
+            break
+
+if __name__ == "__main__":
+    main()
